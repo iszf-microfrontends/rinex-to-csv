f37055c86327656f69aa3005844f14511e31e9dc
Sviatoslav <sviatoslavnazin@gmail.com>
2023-07-02 02:54:33 +0800

    add mantine ui

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,7 @@
-export function App(): JSX.Element | null {
+import { withProviders } from './providers';
+
+function App(): JSX.Element | null {
   return <div>Hello! I am Rinex-to-csv</div>;
 }
+
+export const AppWithProviders = withProviders(App);
--- src/remote-entry.ts
+++ src/remote-entry.ts
@@ -1,1 +1,1 @@
-export { App as default } from './app';
+export { AppWithProviders as default } from './app';

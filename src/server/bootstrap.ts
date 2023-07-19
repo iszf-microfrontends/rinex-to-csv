97a6ba0510809b002d2a25a23d3cb4650f58bd28
Sviatoslav <sviatoslavnazin@gmail.com>
2023-07-19 13:24:40 +0800

    add docker-coompose.yml

--- src/server/bootstrap.ts
+++ src/server/bootstrap.ts
@@ -2,9 +2,30 @@ import { execSync } from 'child_process';
 import express from 'express';
 import path from 'path';
 
-const config = process.env;
+type Config = {
+  PORT: number;
+  MICROFRONTEND_NAME: string;
+  DOMAIN: string;
+  MODULE_FEDERATION_SCOPE: string;
+  MAIN_EXPOSED_COMPONENT: string;
+  BACKEND_NAME: string;
+  MICROFRONTEND_CONTROL_SERVER_URL: string;
+  MSC_CONNECTION_RETRY_DELAY: number;
+};
+
+const config = {
+  PORT: process.env.PORT ?? 3000,
+  MICROFRONTEND_NAME: process.env.MICROFRONTEND_NAME,
+  DOMAIN: process.env.DOMAIN,
+  MODULE_FEDERATION_SCOPE: process.env.MODULE_FEDERATION_SCOPE,
+  MAIN_EXPOSED_COMPONENT: process.env.MAIN_EXPOSED_COMPONENT,
+  BACKEND_NAME: process.env.BACKEND_NAME,
+  MICROFRONTEND_CONTROL_SERVER_URL: process.env.MICROFRONTEND_CONTROL_SERVER_URL,
+  MSC_CONNECTION_RETRY_DELAY: 10000,
+} as Config;
 
 const app = express();
+let isMscConnected = false;
 
 app.use(express.static(path.resolve(__dirname, '..', '..', 'dist/client')));
 
@@ -41,21 +62,23 @@ function connectMicrofrontendToMSC() {
     execSync(
       `curl -s -X POST -H "Content-Type: application/json" -d "${body}" ${config.MICROFRONTEND_CONTROL_SERVER_URL}/api/v1/microfrontends/start`,
     );
+    isMscConnected = true;
     console.log('Microfrontend successfully connected to MCS');
   } catch (error) {
     console.log(`Failed connecting microfrontend to MCS: ${error}`);
-    throw error;
+    setTimeout(() => connectMicrofrontendToMSC(), config.MSC_CONNECTION_RETRY_DELAY);
   }
 }
 
 function disconnectMicrofrontendFromMSC() {
   try {
-    execSync(
-      `curl -s -X GET -H "Content-Type: application/json" ${config.MICROFRONTEND_CONTROL_SERVER_URL}/api/v1/microfrontends/close?name=${config.MICROFRONTEND_NAME}`,
-    );
+    if (isMscConnected) {
+      execSync(
+        `curl -s -X GET -H "Content-Type: application/json" ${config.MICROFRONTEND_CONTROL_SERVER_URL}/api/v1/microfrontends/close?name=${config.MICROFRONTEND_NAME}`,
+      );
+    }
     console.log('Microfrontend successfully disconnected from MCS');
   } catch (error) {
     console.log(`Failed disconnecting microfrontend from MCS: ${error}`);
-    throw error;
   }
 }
